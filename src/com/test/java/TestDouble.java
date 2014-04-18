4b32a562b6359ef281a70e7d094e6099c6f0eded
yuan123h <yuanhuan@192.168.1.107>
2014-04-18 15:12:19 +0800

    double 类型的打印

--- src/com/test/java/TestDouble.java
+++ src/com/test/java/TestDouble.java
@@ -1,5 +1,9 @@
 package com.test.java;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import org.junit.Test;
 
 public class TestDouble {
@@ -8,5 +12,30 @@ public class TestDouble {
 		Double a = 0.0;
 		if (a == 0)
 			System.out.println("a");
+	}
+
+	@Test
+	public void testDivide1() {
+		DecimalFormat df = new DecimalFormat("#.00");
+		System.out.println(df.format(10 / 3.0));
+	}
+
+	@Test
+	public void testDivide2() {
+		System.out.println(String.format("%.2f", new Double(10.0 / 7)));
+	}
+
+	@Test
+	public void testDivide3() {
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMaximumFractionDigits(2);
+		System.out.println(nf.format(20 / 3.0));
+	}
+
+	@Test
+	public void testDivide4s() {
+		BigDecimal bg = new BigDecimal(2/3.0);
+		double f1 = bg.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		System.out.println(f1);
 	}
 }
