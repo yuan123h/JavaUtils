8777f63676049f2a1353763813eaf5894c60acd7
yuan123h <yuanhuan@192.168.1.113>
2014-04-30 15:06:32 +0800

    多线程

--- src/com/test/java/thread/MulThread.java
+++ src/com/test/java/thread/MulThread.java
@@ -5,30 +5,37 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-
 public class MulThread {
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
 		String resultStr = null;
-		
-		int []typeIds = new int[5];
-		
+
+		int[] typeIds = new int[5];
+
 		BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(20);
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 45, 10, java.util.concurrent.TimeUnit.MINUTES, queue, new ThreadPoolExecutor.CallerRunsPolicy());
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 45, 10,
+				java.util.concurrent.TimeUnit.MINUTES, queue,
+				new ThreadPoolExecutor.CallerRunsPolicy());
 		ArrayList<Future<JSONObject>> futureList = new ArrayList<Future<JSONObject>>();
 		for (final Integer typeId : typeIds) {
-			Future<JSONObject> future = executor.submit(new Callable<JSONObject>() {
-				@Override
-				public JSONObject call() throws Exception {
-					JSONObject jsonObject = null;
-					return jsonObject;
-				}
-			});
+			Future<JSONObject> future = executor
+					.submit(new Callable<JSONObject>() {
+						@Override
+						public JSONObject call() throws Exception {
+							JSONObject jsonObject = null;
+							return jsonObject;
+						}
+					});
 			futureList.add(future);
 		}
 		if (null != futureList) {
@@ -38,7 +45,7 @@ public class MulThread {
 					JSONObject json = future.get();
 					allArray.add(json);
 				} catch (RuntimeException e) {
-					//log.error("insert reportData error");
+					// log.error("insert reportData error");
 					e.printStackTrace();
 				}
 			}
@@ -47,4 +54,15 @@ public class MulThread {
 
 		System.out.println(resultStr);
 	}
+
+	// hbase 多线程实现
+	ExecutorService readPool;
+
+	public void t1() {
+		readPool = Executors.newFixedThreadPool(3, new ThreadFactoryBuilder()
+				.setNameFormat("demon").setDaemon(true).build());
+		Thread featch = new Thread();
+		readPool.execute(featch);
+	}
+
 }
